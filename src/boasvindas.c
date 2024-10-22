1399a127119ac85c723a2121a9261b3d062e9513
murillo mendonca mascarenha <mmendoncamascarenha@gmail.com>
2024-10-22 17:27:23 -0300

    atividade sobre binarios e outras coisas

---
+++ src/boasvindas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    // adicionar um pequeno cabeçalho
+    printf("-------------- Programa mensagem ----------------------");
+    printf("        \nSeja bem Vindo ao nosso programa\n");
+    printf("-----------------------fim-----------------------------\n");
+
+    return 0;
+}
---
+++ src/hello.c
@@ -0,0 +1,11 @@
+// Importação de biblioteca padrão para o uso do teclado e monitor
+#include <stdio.h>
+
+int main() {
+    // para exibir uma mensagem na tela iremos usar
+    // o comando printf (exibir formatando)
+    printf("Hello, Word!\n");
+    // informar a o compilador que o programa 
+    // acabou 
+    return 0;
+}
