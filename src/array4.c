f836b0e1f63dca251a268620685c622450eacb91
murillo mendonca mascarenha <mmendoncamascarenha@gmail.com>
2024-11-01 17:26:44 -0300

    atividade do array

---
+++ src/array4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(){
+    int n[]={33,8,7,11,54,77,13,16};
+    int p;
+
+    
+}
---
+++ src/array5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int n[]={13,8,23,95,47,14,10,45,165};
+    int soma = 0;
+    int rs;
+
+
+
+}
